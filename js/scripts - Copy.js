0372206ef9933128f3bbec5ef2a0487175b3ae7a
dzdykes <dzdykes88@gmail.com>
2016-11-29 22:30:30 -0800

    mostly done... mostly

---
+++ js/scripts - Copy.js
@@ -0,0 +1,121 @@
+﻿var order = {
+}
+
+function getRadioSelected(Name) {
+
+}
+
+function SizePrice() {
+    var elements = document.getElementsByName("Size");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            order.size = elements[i].id;
+            return parseFloat(elements[i].value);
+        }
+    }
+}
+
+function MeatPrice() {
+    var price = 0;
+    var Meats = [];
+    var elements = document.getElementsByName("Meat");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            price += 2.0;
+            Meats.push(elements[i].value)
+        }
+    }
+    order.meats = Meats;
+    return price;
+}
+
+function CheesePrice() {
+
+    var cheeseElements = document.getElementsByName("Cheese");
+    for (var i = 0; i < cheeseElements.length; i++) {
+        if (cheeseElements[i].checked){
+            var cheese = cheeseElements[i].id;
+            if (cheese != "CheeseExtra") {
+                cheese == "CheeseRegular" ? order.cheese = "Regular" : order.cheese = "No Cheese";
+                return 0;
+            }
+            order.cheese = "Extra Cheese"
+        }
+    }
+
+    var sizeElements = document.getElementsByName("Size");
+    for (var i = 0; i < sizeElements.length; i++) {
+        if (sizeElements[i].checked) {
+            var size = sizeElements[i].id;
+        }
+    }
+
+    switch (size) {
+        case "Personal": return 1.0;
+        case "Medium": return 2.0;
+        case "Large": return 3.0;
+        case "X-Large": return 4.0;
+        default: return false;
+    }
+    return false;
+}
+
+function CrustPrice () {
+    var elements = document.getElementsByName("Crust");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            order.crust = elements[i].value;
+            if (elements[i].id == "CrustStuffed") {
+                order.crust = "Cheese Stuffed Crust";
+                return 3.0;
+            } else {
+                return 0;
+            }
+        }
+    }
+    return false;
+}
+
+function VeggiesPrice() {
+    var elements = document.getElementsByName("Veggies");
+    var Veggies = [];
+    var price = 0;
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            Veggies.push(elements[i].value);
+            price += 2.0;
+        }
+    }
+    order.veggies = Veggies;
+    return price;
+}
+
+function GetPrice() {
+    var price = 0.0;
+    console.log(price);
+    price += SizePrice();
+    console.log(price);
+    price += CrustPrice();
+    console.log(price);
+    price += MeatPrice();
+    console.log(price);
+    price += CheesePrice();
+    console.log(price);
+    price += VeggiesPrice();
+    order.price = price;
+    GetReceiptInfo();
+}
+
+function GetReceiptInfo() {
+    document.getElementById("Size").innerHTML = order.size;
+    document.getElementById("SizePrice").innerHTML = "$"+SizePrice();
+    document.getElementById("Meats").innerHTML = order.meats;
+    document.getElementById("MeatPrice").innerHTML = "$" + (MeatPrice());
+    document.getElementById("Cheese").innerHTML = order.cheese;
+    document.getElementById("CheesePrice").innerHTML = "$" + CheesePrice();
+    document.getElementById("Crust").innerHTML = order.crust;
+    document.getElementById("CrustPrice").innerHTML = "$" + CrustPrice();
+    document.getElementById("Veggies").innerHTML = order.veggies;
+    document.getElementById("VeggiesPrice").innerHTML = "$" + VeggiesPrice();
+    document.getElementById("TotalPrice").innerHTML = "$" + order.price;
+}
---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+﻿
+function getInputSelected(Name, attr) {
+    var elements = document.getElementsByName(Name);
+    var result = [];
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            attr == "id" ? result.push(elements[i].id) : result.push(elements[i].value);
+        }
+    }
+    return result;
+}
+
+function GetOrder() {
+    var order = {};
+    order.price = getInputSelected("Size")
+    order.size = getInputSelected("Size", "id");
+    order.meat = getInputSelected("Meat");
+    order.cheese = getInputSelected("Cheese");
+    order.crust = getInputSelected("Crust");
+    order.sauce = getInputSelected("Sauce");
+    order.veggies = getInputSelected("Veggies");
+
+    return order;
+}
+
+function float(f) {
+    return parseFloat(f);
+}
+
+function GetExtraCheesePrice(cheese, size) {
+    if (cheese == "Extra Cheese") {
+        switch (size) {
+            case "Personal":
+                return 1.0;
+            case "Medium":
+                return 2.0;
+            case "Large":
+                return 3.0;
+            case "Extra Large":
+                return 4.0;
+        }
+    } else {
+        return 0.0;
+    }
+}
+
+function GetPriceDetails(order) {
+    console.log(order);
+    var priceDetails = {
+        base: float(order.price[0]),
+        meat: float(order.meat.length * 2.0),
+        crust: 0.0,
+        veggies: float(order.veggies.length * 2.0),
+        cheese: GetExtraCheesePrice(order.cheese, order.size),
+        total: 0.0
+    };
+    if (order.crust == "Stuffed Cheese Crust") priceDetails.crust = float(3.0);
+    var total = 0.0;
+    for (var k in priceDetails) {
+        total += priceDetails[k];
+    }
+    priceDetails.total = total;
+    console.log(priceDetails);
+    return priceDetails;
+}
+
+function GetReceiptInfo() {
+    var element = document.getElementById("OrderSummary");
+    element.style = "display: block";
+    var order = GetOrder();
+    var details = GetPriceDetails(order);
+    document.getElementById("Size").innerHTML = order.size;
+    document.getElementById("SizePrice").innerHTML = "$" + details.base;
+    order.meat.length != 0 ?
+        document.getElementById("Meats").innerHTML = order.meat : document.getElementById("Meats").innerHTML = "No Meats";
+    document.getElementById("MeatPrice").innerHTML = "$" + details.meat;
+    document.getElementById("Cheese").innerHTML = order.cheese;
+    document.getElementById("CheesePrice").innerHTML = "$" + details.cheese;
+    document.getElementById("Crust").innerHTML = order.crust;
+    document.getElementById("CrustPrice").innerHTML = "$" + details.crust;
+    order.veggies.length != 0 ?
+        document.getElementById("Veggies").innerHTML = order.veggies : document.getElementById("Veggies").innerHTML = "No Veggies";    
+    document.getElementById("VeggiesPrice").innerHTML = "$" + details.veggies;
+    document.getElementById("TotalPrice").innerHTML = "$" + details.total;
+}
